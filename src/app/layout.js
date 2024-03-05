abb88bc741c80a0b6502ac8a3964b4d8a06e7792
Khaled Mohamed Mowafy <45627560+KhaledMowafy@users.noreply.github.com>
2024-03-05 14:25:04 +0200

    feat(mobile nav): add mobile navigation burger menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/navbar/Nav";
+import MobileNav from "@/components/navbar/MobileNav";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <div className="hidden md:fixed">
       <Nav/>
+      </div>
+      <div className="fixed md:hidden">
+      <MobileNav/>
+      </div>
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/components/navbar/MobileNav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+import Links from '../atoms/Links'
+import Button from '../atoms/Button'
+
+function MobileNav() {
+  return (
+    <div className="w-full fixed top-0 bg-white flex py-3 px-8 justify-between">
+    <div className="flex w-4/12 justify-around items-center">
+    <Image src="./menu.svg" width={24} height={24} alt="logo"/>
+        <Image src="./logoQR.svg" width={108} height={43} alt="logo"/>
+    </div>
+    <div className="flex w-5/12 justify-around items-center">
+        <Image className="cursor-pointer" src="./search-normal.svg" width={24} height={24} alt="logo"/>
+        <Image className="cursor-pointer" src="./notification.svg" width={24} height={24} alt="logo"/>
+        <Image className="cursor-pointer" src="./prof.svg" width={40} height={40} alt="logo"/>
+      
+    </div>
+    </div>
+  )
+}
+
+export default MobileNav
